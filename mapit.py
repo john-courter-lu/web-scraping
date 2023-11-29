3bb3af038ae6e59d6784e1a37e0d1744cb74d4a6
John Lu <johnlu2006@gmail.com>
2023-11-28 21:35:19 -0600

    use Pylint to follow linting rules

--- mapit.py
+++ mapit.py
@@ -1,7 +1,9 @@
 #! python3
 # mapIt.py - Launches a map in the browser using an address from the
 # command line or clipboard.
-import webbrowser, sys, pyperclip
+import webbrowser
+import sys
+import pyperclip
 if len(sys.argv) > 1:
     # Get address from command line.
     address = ' '.join(sys.argv[1:])
